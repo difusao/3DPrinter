5fc3a4467c9549732c378de8a9d532829a8b5e2f
unknown <difusao@gmail.com>
2020-03-31 00:39:45 -0300

    update files config

--- cura/plugins/PostProcessingPlugin/scripts/DisplayFilenameAndLayerOnLCD_ByDifusao.py
+++ cura/plugins/PostProcessingPlugin/scripts/DisplayFilenameAndLayerOnLCD_ByDifusao.py
@@ -12,14 +12,14 @@
 from ..Script import Script
 from UM.Application import Application
 
-class DisplayFilenameAndLayerOnLCD(Script):
+class DisplayFilenameAndLayerOnLCD_ByDifusao(Script):
     def __init__(self):
         super().__init__()
 
     def getSettingDataString(self):
         return """{
-            "name": "Exibir arquivo e camadas no LCD",
-            "key": "DisplayFilenameAndLayerOnLCD",
+            "name": "Exibir arquivo e camadas no LCD (by Difusão)",
+            "key": "DisplayFilenameAndLayerOnLCD_ByDifusao",
             "metadata": {},
             "version": 2,
             "settings":
@@ -61,9 +61,9 @@ class DisplayFilenameAndLayerOnLCD(Script):
         max_layer = 0
 
         if self.getSettingValueByKey("name") != "":
-            name = self.getSettingValueByKey("name")
+            name = self.getSettingValueByKey("name") + ":"
         else:
-            name = Application.getInstance().getPrintInformation().jobName
+            name = Application.getInstance().getPrintInformation().jobName + ":"
 
         if not self.getSettingValueByKey("scroll"):
             if self.getSettingValueByKey("maxlayer"):
@@ -73,15 +73,19 @@ class DisplayFilenameAndLayerOnLCD(Script):
         else:
             #lcd_text = "M117 Printing " + name + " - Layer "
             lcd_text = "M117 " + name + " Camada "
+            
         i = self.getSettingValueByKey("startNum")
+        
         for layer in data:
             display_text = lcd_text + str(i)
             layer_index = data.index(layer)
             lines = layer.split("\n")
+            
             for line in lines:
                 if line.startswith(";LAYER_COUNT:"):
                     max_layer = line
                     max_layer = max_layer.split(":")[1]
+                    
                 if line.startswith(";LAYER:"):
                     if self.getSettingValueByKey("maxlayer"):
                         display_text = display_text + "/" + max_layer
@@ -90,6 +94,7 @@ class DisplayFilenameAndLayerOnLCD(Script):
                     line_index = lines.index(line)
                     lines.insert(line_index + 1, display_text)
                     i += 1
+                    
             final_lines = "\n".join(lines)
             data[layer_index] = final_lines
             
